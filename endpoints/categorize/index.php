d639818788655fa9c44172c9331d26fcb3794d4e
cgs <cgs@cgs-server.loc>
2025-05-11 17:53:30 +0000

    changed categorize endpoints and categorize model to built in

--- endpoints/categorize/index.php
+++ endpoints/categorize/index.php
@@ -4,17 +4,13 @@ include "../../functions.php";
 
 INIT_BACKEND_CALL();
 
+onGET(function()
+{
+    include "get.php";
+    return;
+});
+
 onPOST(function() {
-    include "add.php";
-    return;
-});
-
-onDELETE(function() {
-    include "delete.php";
-    return;
-});
-
-onPUT(function() {
-    include "update.php";
+    include "modify.php";
     return;
 });
